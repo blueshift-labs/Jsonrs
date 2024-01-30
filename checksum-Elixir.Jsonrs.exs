17052c3fa595e61746c8c6212a32ab4a270f2b8b
yang <yang@blueshift.com>
2024-01-30 00:41:31 -0800

    checksum

---
+++ checksum-Elixir.Jsonrs.exs
@@ -0,0 +1,12 @@
+%{
+  "jsonrs-v0.4.1-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:1529ae2ac3927afca9bc3287925c30e597b438f8b0c4b0a147e79eedc27be3e7",
+  "jsonrs-v0.4.1-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:c5d3fcdeaf00633f55bb9832d2f3f9d5a0d9929546c762ef87fb15ddf475207f",
+  "libjsonrs-v0.4.1-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:48608af54f71eac58075d200133b291cd41c054296e88bfad6bea6e0698a241f",
+  "libjsonrs-v0.4.1-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:3f97584aee7070c3c7cfc286ca1f889edd0ce89669a124c089b5ac79f74b942b",
+  "libjsonrs-v0.4.1-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:c760ce3e8928eba628a0cc1b573f9f8b1de877d974bfc8a8a9e63e170e7bb1f3",
+  "libjsonrs-v0.4.1-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:2e18a52ef86cbdc8d6d2df873012e8a3a9470c25ccad91839ce9ae08ede132fe",
+  "libjsonrs-v0.4.1-nif-2.15-riscv64gc-unknown-linux-gnu.so.tar.gz" => "sha256:e10b427b8d3f418dc4bab4fe83f2d5b1a79541f9a5b129e92a1891b07d6b401c",
+  "libjsonrs-v0.4.1-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:098cad02e665585678bd800716b0bdb877e6e9607b71e33bc311adc341df1ea8",
+  "libjsonrs-v0.4.1-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:78ec233804150c6089815530c97da256834c4e808f0168a22840ae9c7bb3376a",
+  "libjsonrs-v0.4.1-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:25dafb07346c49ffeca3d7e3f839f02e7cb07371e8c379c74166f712a37d6c46",
+}
