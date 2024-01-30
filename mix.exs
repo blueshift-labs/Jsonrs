35ad7fc68e546789944fe965005636cbd3fdb811
yang <yang@blueshift.com>
2024-01-29 16:06:30 -0800

    strictly enforce protocol impl

--- mix.exs
+++ mix.exs
@@ -1,19 +1,20 @@
 defmodule Jsonrs.MixProject do
   use Mix.Project
 
-  @version "0.3.3"
+  @version "0.3.6"
 
   def project do
     [
       app: :jsonrs,
       version: @version,
       elixir: "~> 1.9",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: description(),
       package: package(),
       docs: docs(),
-      source_url: "https://github.com/benhaney/jsonrs"
+      source_url: "https://github.com/blueshift-labs/jsonrs"
     ]
   end
 
@@ -24,12 +25,17 @@ defmodule Jsonrs.MixProject do
     ]
   end
 
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["test/support", "lib"]
+  defp elixirc_paths(_), do: ["lib"]
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:decimal, "~> 1.0 or ~> 2.0", optional: true},
       {:rustler_precompiled, "~> 0.7.0"},
       {:rustler, "~> 0.30.0", optional: true, runtime: false},
-      {:ex_doc, "~> 0.30", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.30", only: :dev, runtime: false}
     ]
   end
 
@@ -41,10 +47,19 @@ defmodule Jsonrs.MixProject do
 
   defp package() do
     [
-      maintainers: ["Ben Haney"],
+      maintainers: ["Ben Haney", "Yang Ou"],
       licenses: ["Unlicense"],
-      links: %{"GitHub" => "https://github.com/benhaney/jsonrs"},
-      files: ["lib", "mix.exs", "README*", "native/jsonrs/src", "native/jsonrs/.cargo", "native/jsonrs/README*", "native/jsonrs/Cargo*", "checksum-*.exs"]
+      links: %{"GitHub" => "https://github.com/blueshift-labs/jsonrs"},
+      files: [
+        "lib",
+        "mix.exs",
+        "README*",
+        "native/jsonrs/src",
+        "native/jsonrs/.cargo",
+        "native/jsonrs/README*",
+        "native/jsonrs/Cargo*",
+        "checksum-*.exs"
+      ]
     ]
   end
 
@@ -54,7 +69,7 @@ defmodule Jsonrs.MixProject do
       name: "Jsonrs",
       source_ref: "v#{@version}",
       canonical: "http://hexdocs.pm/jsonrs",
-      source_url: "https://github.com/benhaney/jsonrs",
+      source_url: "https://github.com/blueshift-labs/jsonrs",
       extras: [
         "README.md"
       ]
